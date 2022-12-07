0dddfde88774069258ec9fe31dd998fdae4d18f7
TheMVProgrammer <franklin0415@hotmail.com>
2022-12-07 10:52:23 -0400

    Setup and adding some elements

---
+++ src/components/Header/styles.js
@@ -0,0 +1,46 @@
+import { alpha, makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  title2: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+      position: 'absolute',
+      right: 0,
+      marginRight: theme.spacing(38),
+    },
+    [theme.breakpoints.down('sm')]: {
+        display: 'none'
+    }
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': { backgroundColor: alpha(theme.palette.common.white, 0.25) },
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: { marginLeft: theme.spacing(3), width: 'auto' },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none', display: 'flex', alignItems: 'center', justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(4)}px)`, transition: theme.transitions.create('width'), width: '100%', [theme.breakpoints.up('md')]: { width: '20ch' },
+  },
+  toolbar: {
+    display: 'flex', justifyContent: 'space-between',
+  }
+}));
